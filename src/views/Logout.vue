60fad605c10584840c73286f8d82bb71eeaa0a38
ThundaHorse <abeeeeekim@gmail.com>
2019-06-18 12:01:43 -0500

    tentative login/logout pages

---
+++ src/views/Logout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class='logout'>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import axios from 'axios'
+
+export default {
+  created: function() {
+    delete axios.defaults.headers.common["Authorizations"];
+    localStorage.removeItem("jwt");
+
+    alert('Successfully Logged Out!').then(this.$router.push("/"));
+    // this.$router.push("/");
+  }
+};
+</script>
